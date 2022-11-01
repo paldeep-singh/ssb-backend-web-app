bce828ccc09ea478dc94cdf331dc168d99d45545
Paldeep Singh <paldeep@DESKTOP-7TVM8OC>
2022-11-01 22:26:36 +1100

    Add email field

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Typography } from '@mui/material';
 
+export default function BasicTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch', },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="Email" variant="standard" />
+    </Box>
+  );
+}
 ReactDOM.render( 
-  <div>SSB Backend Web App</div>, 
+ <BasicTextFields />, 
   document.getElementById('root')
 );
