b7f78937a7b5502446502053febed529bcb45f8a
Paldeep Singh <paldeep@DESKTOP-7TVM8OC>
2022-11-05 01:37:41 +1100

    Add logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom'
 import { FC } from 'react'
 import './index.css'
+import Logo from './assets/logo.svg'
 
 const EMAIL = 'Email'
 
 export const App: FC = () => {
   return (
     <div className="space-x-2 pt-6 text-center">
+      <img src={Logo} alt="Logo" className="block mx-auto" />
       <label>{EMAIL}</label>
       <input name="email" type="text" className="border-purple-700 border-2" />
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.tsx"),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
-        use: ["babel-loader"],
-        exclude: /node_modules/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
-      },
-    ],
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.html"),
+      template: path.resolve(__dirname, './src/index.html')
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
   devServer: {
-    static: path.join(__dirname, "./src"),
+    static: path.join(__dirname, './src'),
     port: 3164,
-    hot: "only",
+    hot: 'only',
     compress: true,
-    open: true,
-  },
-};
+    open: true
+  }
+}
