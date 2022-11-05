2eaee14c4f41d0c8a3ba89a1d3fab833ae62a634
Paldeep Singh <paldeep@DESKTOP-7TVM8OC>
2022-11-05 23:19:01 +1100

    Add root route

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { FC } from 'react'
 import './index.css'
 import LoginScreen from './features/login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 export const App: FC = () => {
-  return <LoginScreen />
+  const router = createBrowserRouter([{ path: '/', element: <LoginScreen /> }])
+  return <RouterProvider router={router} />
 }
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
