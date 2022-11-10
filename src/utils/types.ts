e68c403363fa3acd9eb192ee0e60eb72aea74a43
Paldeep Singh <paldeep@DESKTOP-7TVM8OC>
2022-11-10 21:20:09 +1100

    Improve session type guard

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,17 +3,16 @@ interface ISessionData {
 }
 
 export interface ISession {
-  sessionId: string
-  sessionData: ISessionData
+  id: string
+  data: ISessionData
 }
 
 export const isSession = (maybeSession: unknown): maybeSession is ISession => {
   return (
     typeof maybeSession === 'object' &&
     maybeSession !== null &&
-    typeof (maybeSession as ISession).sessionId === 'string' &&
-    typeof (maybeSession as ISession).sessionData === 'object' &&
-    (maybeSession as ISession).sessionData !== null &&
-    typeof (maybeSession as ISession).sessionData.userId === 'string'
+    'id' in maybeSession &&
+    'data' in maybeSession &&
+    'userId' in (maybeSession as ISession).data
   )
 }
